33cfc76db3cb764cad93eb80572ddba88df7a82c
iMGamal <mohamed.gamal126027@gmail.com>
2024-10-17 23:25:40 +0000

    Task0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- Command to create table with requirements
-CREATE TABLE users IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS users (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255)
-)
+);
